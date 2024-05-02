a0556f4d11bb5182e38bad41350ed35c87a12dd7
HackXIt <hackxit@gmail.com>
2024-05-03 01:05:10 +0200

    Add basic implementation for generating designs via ChatGPT

--- src/generate.py
+++ src/generate.py
@@ -1,5 +1,7 @@
 import argparse
 from importlib.metadata import metadata
+from os import system
+from pyexpat.errors import messages
 from re import split
 
 implemented_types = ["arc", 
@@ -13,7 +15,7 @@ implemented_types = ["arc",
 
 # Generators
 def capture_random(env: dict, args: argparse.Namespace):
-    import subprocess, os, shutil, glob
+    import subprocess, os, shutil
     from clearml import Task
     import numpy as np
     task = Task.current_task()
@@ -80,6 +82,37 @@ def capture_design(design_folder: str):
     import subprocess, os, shutil
     from clearml import Task
     task = Task.current_task()
+    logger = task.get_logger()
+    design_files = [f for f in os.listdir(design_folder) if os.path.isfile(os.path.join(design_folder, f))]
+    files = []
+    errors = 0
+    for i, design_file in enumerate(design_files):
+        print(f"Running design generator on file {design_file}")
+        gen = subprocess.run([os.path.abspath(env['mpy_path']), os.path.abspath(env['mpy_main']), '-m', 'design', '-o', 'screenshot.jpg', '-f', os.path.abspath(os.path.join(design_folder, design_file)), '--normalize'], cwd=os.path.abspath(os.path.curdir), capture_output=True, text=True)
+        if gen.returncode != 0:
+            print(f"Failed to generate UI from design file {design_file}:\n{gen.stdout}\n{gen.stderr}")
+            errors += 1
+            continue
+        tmp_image = os.path.abspath(os.path.join(os.path.abspath(os.path.curdir), "screenshot.jpg"))
+        tmp_text = os.path.abspath(os.path.join(os.path.abspath(os.path.curdir), "screenshot.txt"))
+        if not os.path.exists(tmp_image) or not os.path.exists(tmp_text):
+            print(f"Failed to find generated UI files from design file {design_file}")
+            errors += 1
+            continue
+        gen_image = os.path.abspath(os.path.join(env['output_folder'], f"ui_{i}.jpg"))
+        gen_text = os.path.abspath(os.path.join(env['output_folder'], f"ui_{i}.txt"))
+        try:
+            shutil.move(tmp_image, gen_image)
+            shutil.move(tmp_text, gen_text)
+        except FileNotFoundError as e:
+            print(f"Failed to move files from design file {design_file}:\n{tmp_image} -> {gen_image}\n{tmp_text} -> {gen_text}\n{e}")
+            errors += 1
+            continue
+        files.append((gen_image, gen_text))
+        logger.report_scalar(title='Generator', series='errors', value=errors, iteration=i)
+    generated_files = len(files)
+    env['generated_files'] = generated_files
+    env['files'] = files
 
 # Dataset helpers
 def replace_class_names(files: list):
@@ -205,7 +238,8 @@ def load_json_file(filepath: str):
     import json
     with open(filepath, 'r') as f:
         return json.load(f)
-def verify_design(design_file: str, schema_file: str) -> bool|tuple[bool, Exception]:
+
+def verify_design_from_file(design_file: str, schema_file: str) -> tuple[bool, Exception]:
     from jsonschema import validate
     from jsonschema.exceptions import ValidationError
     design = load_json_file(design_file)
@@ -213,20 +247,189 @@ def verify_design(design_file: str, schema_file: str) -> bool|tuple[bool, Except
     try:
         validate(instance=design, schema=schema)
         print(f"Provided design file {design_file} is valid.")
-        return True
+        return True, None
     except ValidationError as e:
         print(f"Provided design file {design_file} is invalid:\n{e}")
         return False, e
 
-def generate_designs(env: dict, args: argparse.Namespace):
-    import os
+def verify_design_from_string(design: str, schema_file: str) -> tuple[bool, Exception]:
+    from jsonschema import validate
+    from jsonschema.exceptions import ValidationError
+    schema = load_json_file(schema_file)
+    try:
+        validate(instance=design, schema=schema)
+        return True, None
+    except ValidationError as e:
+        return False, e
+
+
+def ask_gpt(openai: dict):
     from openai import OpenAI
-    from openai.types import ChatModel
-    open_ai = {}
-    if args.model not in ChatModel:
-        print(f"Invalid model name: {args.model}. Valid are:\n{'\n'.join(ChatModel)}")
-        return
     client = OpenAI() # per-default uses api_key=os.environ['OPENAI_API_KEY']
+    if 'temperature' in openai:
+        response = client.chat.completions.create(
+            model=openai['model'],
+            messages=openai['messages'],
+            max_tokens=openai['max_tokens'],
+            response_format={"type": openai['format']},
+            stop=openai['stop'] if 'stop' in openai else None,
+            temperature=openai['temperature'],
+        )
+    elif 'top_p' in openai:
+        response = client.chat.completions.create(
+            model=openai['model'],
+            messages=openai['messages'],
+            max_tokens=openai['max_tokens'],
+            response_format={"type": openai['format']},
+            stop=openai['stop'] if 'stop' in openai else None,
+            top_p=openai['top_p']
+        )
+    else:
+        response = client.chat.completions.create(
+            model=openai['model'],
+            messages=openai['messages'],
+            max_tokens=openai['max_tokens'],
+            response_format={"type": openai['format']},
+            stop=openai['stop'] if 'stop' in openai else None,
+        )
+    # TODO Return the response conditionally based on the choices made or none on failure
+    return response
+
+def ask_gpt_for_design_idea(openai: dict):
+    system_message = """
+You are a UI designer.
+Your goal is to create a new design idea and guideline for an user interface to be given to developers.
+The user will provide a context constraint and goal for the design.
+You must adhere to the design constraints and goals.
+Make sure to include the following in your design idea:
+- A title for the design
+- The context of the design
+- The design goals or objectives
+- The design elements
+- A high-level description of the design
+
+DESIGN RULES:
+In the idea, you will include mentions of design elements that should be present in the form of widgets.
+These are the allowed widget types to be used as design elements and their corresponding purposes:
+- Arc: A circular arc to select or display a value within a range
+- Bar: A horizontal or vertical bar to select or display a value within a range
+- Button: Clickable button with or without text
+- ButtonMatrix: Matrix of buttons with or without text
+- Calendar: A calendar showing the month with or without highlighted dates
+- Checkbox: A box that can be checked or unchecked
+- Dropdown: A box containing multiple options as entries where one can be selected
+- Label: A field for text display
+- LED: A circle with custom styling that can be turned on or off like a light
+- Roller: A special dropdown with a rolling interface for selecting entries
+- Scale: A scale for displaying a range of values
+- Slider: A slider for selecting a value within a range
+- Spinbox: Spinbox widget
+- Switch: A switch for toggling between two states
+- Table: A tabular widget for displaying data
+- TextArea: A field for input or display of multiple text lines
+- Container: A special widget containing other widgets in a structured layout
+
+Design elements can be structurally combined using the special widget 'container'.
+The structure of such a container may be defined as a layout of the following types and purposes:
+- none: Widgets are structured using absolute positioning
+- grid: Widgets are structured in rows and columns, each cell containing a widget
+- flex: Widgets are structured in a flexible layout that adjusts to the available space horizontally or vertically
+
+Design elements can also be styled individually or identical using a style group.
+Style groups define a set of style properties that can be applied to multiple widgets at once.
+The style properties include color, size, padding, margin, opacity, border, lines, shadows and more.
+Such style groups are named using a unique identifier and may be applied to multiple widgets, but each widget only can have one style group applied.
+
+TASK:
+It is your task to create a design idea for a user interface that adheres to these design rules.
+"""
+    initial_prompt = """
+Create a new UI design in the context of an embedded environment with a single display.
+"""
+    openai['messages'] = [
+        {"role": "system", "content": system_message},
+        {"role": "user", "content": initial_prompt}
+    ]
+    openai['format'] = "text"
+    # Get response
+    response = ask_gpt(openai)
+    return response.choices[0].message.content
+
+def ask_gpt_for_design_json(openai: dict, design: dict):
+    import os, json
+    schema_file = os.path.join(os.path.curdir, 'schema', 'design_file.schema.json')
+    openai['schema'] = json.load(open(schema_file, 'r'))
+    system_message = """
+You are a UI generator.
+Your goal is to create a new single window UI using a specialized JSON format.
+The format specification is available in the design.schema.json file.
+Follow the design guideline of the user when generating the UI.
+Output a valid JSON object that represents the UI design.
+    """
+    system_message += f"\ndesign.schema.json:\n{str(openai['schema'])}"
+    initial_prompt = "Create a new UI design from this design idea:\n"
+    if len(design['errors']) == 0:
+        openai['messages'] = [
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": initial_prompt + design['idea']},
+        ]
+    else:
+        # NOTE This is a bit of a hack using only the last error message (which might result in error repetition)
+        error_prompt = "The design has validation errors:\n" + str(design['errors'][-1]) + "\nCorrect ALL errors and generate a complete JSON output."
+        openai['messages'] = [
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": initial_prompt + design['idea']},
+            {"role": "assistant", "content": design['json_raw']},
+            {"role": "user", "content": error_prompt}
+        ]
+    openai['format'] = "json_object"
+    # Get response
+    response = ask_gpt(openai)
+    json_design = response.choices[0].message.content
+    json_file = os.path.join(os.path.curdir, 'tmp', f"design_{design['attempts']}.json")
+    with open(json_file, 'w') as f:
+        f.write(json_design)
+    # Check if response is valid JSON
+    is_valid, error = verify_design_from_file(json_file, schema_file) # NOTE Ignoring the error, could be useful for debugging though
+    return is_valid, json_design, error
+
+def generate_designs(env: dict, args: argparse.Namespace):
+    import os, json
+    # TODO should connect stuff to ClearML task
+    open_ai = {}
+    os.makedirs(args.output_folder, exist_ok=True)
+    env['design_folder'] = args.output_folder
+    open_ai['model'] = args.model
+    open_ai['max_tokens'] = args.max_tokens
+    if hasattr(args, 'temperature'):
+        open_ai['temperature'] = args.temperature
+    elif hasattr(args, 'top_p'):
+        open_ai['top_p'] = args.top_p
+    for i in range(args.designs):
+        print(f"Generating design {i}...")
+        valid_design = False
+        design = {}
+        design['attempts'] = 0
+        design['errors'] = []
+        design['idea'] = ask_gpt_for_design_idea(open_ai)
+        print(f"Idea: {design['idea']}")
+        while not valid_design and design['attempts'] < 3: # NOTE Limiting attempts to 5 (limiting costs for now)
+            valid_design, design['json_raw'], error = ask_gpt_for_design_json(open_ai, design)
+            if error:
+                design['errors'].append(error)
+                design['attempts'] += 1
+                print(f"JSON generation failed with error.")
+                with open(os.path.join(os.path.curdir, 'tmp', f"design_{i}_error.txt"), 'w+') as f:
+                    f.write(str(error))
+        else:
+            msg_fail = f"Failed to generate valid design after {design['attempts']} attempts"
+            msg_success = f"Generated valid design after {design['attempts']}"
+            print(msg_fail if not valid_design else msg_success)
+        print(f"Design JSON:\n{design['json_raw']}")
+        design['file'] = os.path.join(args.output_folder, f"design_{i}.json")
+        with open(design['file'], 'w') as f:
+            json.dump(design['json_raw'], f)
+        open_ai[f'design_{i}'] = design
     env['OPEN_AI'] = open_ai
 
 # CLI helpers
@@ -252,7 +455,7 @@ class _HelpAction(argparse._HelpAction):
 def cli_parser():
     parser = argparse.ArgumentParser(description='Generate UI Detector dataset', add_help=False, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-h', '--help', action=_HelpAction, help='show this help message and exit') # add custom help
-    parser.add_argument('-o', '--output', type=str, default='output', help='Output folder')
+    parser.add_argument('-o', '--output', type=str, default='tmp/output', help='Output folder')
     parser.add_argument('--mpy-path', type=str, default='', help='Path to MicroPython binary')
     parser.add_argument('--mpy-main', type=str, default='', help='Path to main.py of micropython script')
     parser.add_argument('-d', '--dataset', type=str, default='', help='Name of the dataset')
@@ -272,9 +475,9 @@ def cli_parser():
     design_remote = design_variant.add_parser('remote', help='Generate UIs from remote design files')
     # TODO Add arguments for remote design generator
     design_gpt = design_variant.add_parser('gpt', help='Generate UIs using ChatGPT API')
-    design_gpt.add_argument('--api-key', type=str, required=True, help='ChatGPT API key')
+    design_gpt.add_argument('--api-key', type=str, default='', help='ChatGPT API key')
     design_gpt.add_argument('--model', type=str, required=True, help='ChatGPT model name')
-    design_gpt.add_argument('--max-tokens', type=int, default=255, help=f'ChatGPT maximum tokens') # TODO Check what would be sensible amount of default tokens
+    design_gpt.add_argument('--max-tokens', type=int, default=2500, help=f'ChatGPT maximum tokens') # TODO Check what would be sensible amount of default tokens
     design_gpt.add_argument('--designs', type=int, default=10, help='Number of designs to generate')
     design_gpt.add_argument('-o', '--output-folder', type=str, default='tmp', help='Output folder for generated designs')
     gpt_arg = design_gpt.add_mutually_exclusive_group()
@@ -312,7 +515,7 @@ def validate_cli_args(args: dict):
         raise NotImplementedError("Remote design generator not implemented yet")
         pass
     if args.type == 'design' and args.variant == 'gpt':
-        if not args.api_key:
+        if not args.api_key and 'OPENAI_API_KEY' not in os.environ:
             print('ChatGPT API key not provided')
             has_error = True
         if not args.model:
@@ -320,9 +523,6 @@ def validate_cli_args(args: dict):
             has_error = True
     if has_error:
         sys.exit(1)
-
-def verify_cli_args(args: dict):
-    import os, sys
 
 if __name__ == '__main__':
     parser = cli_parser()
@@ -334,12 +534,12 @@ if __name__ == '__main__':
         capture_random(env, args)
     elif args.type == 'design':
         if args.variant == 'local':
-            capture_design(env, args)
+            capture_design(env['output_folder'])
         elif args.variant == 'remote':
             pass
         elif args.variant == 'gpt':
             generate_designs(env, args)
-            capture_design(env, args)
+            capture_design(env['output_folder'])
     prepare_dataset(env, args)
     if not args.no_dataset:
         create_dataset(env, args)
