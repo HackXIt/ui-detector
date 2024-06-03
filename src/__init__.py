3627fc445a16ac6796622745c0d84d655168214c
HackXIt <hackxit@gmail.com>
2024-06-03 21:38:52 +0200

    Fix image path issue in pdoc

--- src/__init__.py
+++ src/__init__.py
@@ -4,4 +4,7 @@ README_path = os.path.join('README.md')
 with open(README_path, 'r') as f:
     README = f.read()
 
+# Fix the path of images in the READM: https://github.com/mitmproxy/pdoc/issues/696
+README.replace('./docs/assets/', './assets/')
+
 __doc__ = README
